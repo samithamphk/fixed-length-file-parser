07d2db04f2fa4e7b800be7216ec07b59a4e283fc
samithamphk <4257445+samithamphk@users.noreply.github.com>
2020-09-01 10:35:49 +0800

    added tests for generator

--- files/generator.py
+++ files/generator.py
@@ -5,56 +5,86 @@ from files import helper
 
 
 def read_file_specification(path, is_print=True):
+    """
+    Return a dictionary by reading a fixed length file specification in json.
+    :param path:
+    :param is_print: print out the contents if True
+    :return:
+    """
     spec_file = helper.read_json_file(path)
     if is_print:
         print(spec_file)
     return spec_file
 
 
-
 def create_fixed_length_word(word, offset, filler, is_header=False):
+    """
+    Generate a fixed length word (for a given row,column) according to the offset limits.
+    :param word: Column name from the file specification. '' otherwise
+    :param offset: collumn offset from the specification
+    :param filler: filling character for fixed length word. defaults to ' '
+    :param is_header:
+    :return: string
+    """
     if is_header:
-        fillpos = offset - len(word)  # fillpos is the number of places to fill for a cell
-        if fillpos > 0:
-            word += filler * fillpos
+        fill_pos = offset - len(word)  # fill_pos is the number of places to fill for a cell
+        if fill_pos > 0:
+            word += filler * fill_pos  # create fixed length header word
     else:
-        ran_num = random.randint(1, offset)
-        word = string.ascii_lowercase[0:ran_num]
-        fillpos = offset - ran_num
-        if fillpos > 0:
-            word += filler * fillpos
+        ran_num = random.randint(1, offset)  # 1<= random number <= offset
+        word = string.ascii_lowercase[0:ran_num]  # extract the first ran_num long string from the alphabet
+        fill_pos = offset - ran_num
+        if fill_pos > 0:
+            word += filler * fill_pos  # creates a fixed length word
     return word
 
 
 def create_a_row(row_num, filler, spec):
-    row_list = list()
+    """
+    Generate a fixed length string for a row in the fixed length file.
+    :param row_num: 0 for the header, anything else is a data row
+    :param filler: filler character
+    :param spec: dictionary specifying fixed length file
+    :return: fixed length string
+    """
+    row_list = list()  # list to hold the fixed length words in a row
     for col in range(0, len(spec['ColumnNames'])):
         word = ''
-        offset = int(spec['Offsets'][col])
+        offset = int(spec['Offsets'][col])  # read the offset value from the file specification
         if row_num == 0:  # create a word in header
             word = create_fixed_length_word(spec['ColumnNames'][col], offset, filler, True)
         else:  # create a word in a regular row
             word = create_fixed_length_word('', offset, filler)
-        row_list.append(word)  # add cell to a row
-    row = ''.join(row_list)
+        row_list.append(word)
+    row = ''.join(row_list)  # create row string
     return row
 
 
 def generate_fixed_length_content(numlines, spec, filler=' '):
-    out_list = list()  # list of lists as the primary data structure for the content
+    """
+    Generate file content of given number or rows.
+    :param numlines: user provided number to determine file size
+    :param spec: fixed length file specification dictionary
+    :param filler: filler character for content
+    :return: list of list
+    """
+    out_list = list()  # list of lists as the primary data structure to hold the content
     for row_num in range(0, numlines + 1):
         row = create_a_row(row_num, filler, spec)
-        out_list.insert(row_num, row)  # add row to the list of lists
+        out_list.insert(row_num, row)
     return out_list
 
 
-def save_fixed_length_file(output_path, content, eol=None):
-    with open(file=output_path, mode='w', encoding='windows-1252') as fwf:
+def save_fixed_length_file(output_path, content, encoding, eol=None):
+    """
+    Write a fixed length file from the given content
+    :param output_path:
+    :param content: list of list
+    :param encoding: defaults to windows-1252 from the file specification
+    :param eol: defaults to ' '
+    """
+    with open(file=output_path, mode='w', encoding=encoding) as fwf:
         for line in content:
             fwf.write(line)
             if eol is not None:
-                fwf.write(eol)  # usually '\n' for end of line
-
-
-
-
+                fwf.write(eol)
--- files/test_generator.py
+++ files/test_generator.py
@@ -1,28 +1,43 @@
 import unittest
 from files import helper, generator
 
+
 class GeneratorTestCase(unittest.TestCase):
     def setUp(self):
         self.defaults = helper.read_json_file('../static/app_defaults.json')
         self.spec = generator.read_file_specification(self.defaults['specification_file_path'], True)
 
     def tearDown(self):
-        self.defaults=None
-        self.spec=None
+        self.defaults = None
+        self.spec = None
 
     def test_generate_content_size(self):
         num_lines = self.defaults['numlines']
         output_text = generator.generate_fixed_length_content(numlines=num_lines, spec=self.spec)
-        self.assertEqual(len(output_text)-1, num_lines)# minus 1 for header
+        self.assertEqual(len(output_text) - 1, num_lines)  # minus 1 for header
 
     def test_generate_content_create_rows(self):
         num_lines = self.defaults['numlines']
         output_text = generator.generate_fixed_length_content(numlines=num_lines, spec=self.spec)
         self.assertEqual(len(output_text) - 1, num_lines)  # minus 1 for header
 
+    def test_create_a_data_row_len(self):
+        row_number = 1
+        row = generator.create_a_row(row_num=row_number, filler=' ', spec=self.spec)
+        self.assertEqual(len(row), sum(map(int, self.spec["Offsets"])))
+
+    def test_create_a_data_row_filler(self):
+        row_number = 1
+        row = generator.create_a_row(row_num=row_number, filler=' ', spec=self.spec)
+        offsets = map(int, self.spec["Offsets"])
+
+        t1 = offsets[0]
+        t2 = sum(offsets[0:2])
+        t3 = sum(offsets[0:3])
+        t4 = sum(offsets[0:4])
+
+        self.assertTrue(row[t1] == ' ' or row[t2] == ' ' or row[t3] == ' ' or row[4] == ' ')
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
